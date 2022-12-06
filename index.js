613afca19a1a2f2371a3c7481b7cc3965c645393
skandog <muumrar@gmail.com>
2022-12-06 16:50:48 +0000

    completed part 2 and provided correct solution. this challenge hurt my head

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-var puzzleInput = "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2";
+import puzzleInput from "./puzzleInput.js";
+// // Dummy data
+// const puzzleInput = `    [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2`;
+// Sorting toers into arrays
 var cols = puzzleInput.split("\n\n")[0].split("\n");
-var colsArrays = new Array(cols.length - 1);
-for (var i = 0; i < cols.length - 1; i++) {
+var colsArrays = new Array(cols.length);
+for (var i = 0; i < cols.length; i++) {
     colsArrays[i] = [];
-}
-var instructions = puzzleInput.split("\n\n")[1].split("\n");
-var digitInstructions = [];
-for (var i = 0; i < instructions.length; i++) {
-    var numberToMove = Number(instructions[i].charAt(5));
-    var location_1 = Number(instructions[i].charAt(12));
-    var target = Number(instructions[i].charAt(17));
-    digitInstructions[i] = [numberToMove, location_1, target];
 }
 var numberOfColumns = Number(cols[cols.length - 1]
     .replace(/\s/g, "")
     .charAt(cols[cols.length - 1].replace(/\s/g, "").length - 1));
-// for (let i = 0; i < cols.length - 1; i++) {
-//   if (cols[i].charAt(1) !== " ") {
-//     colsArrays[0].unshift(cols[i].charAt(1));
-//   }
-//   if (cols[i].charAt(5) !== " ") {
-//     colsArrays[1].unshift(cols[i].charAt(5));
-//   }
-//   if (cols[i].charAt(9) !== " ") {
-//     colsArrays[2].unshift(cols[i].charAt(9));
-//   }
-// }
 // sorts columns into arrays
 for (var i = 0; i < cols.length - 1; i++) {
     for (var j = 0; j < numberOfColumns; j++) {
@@ -35,13 +26,30 @@ for (var i = 0; i < cols.length - 1; i++) {
         }
     }
 }
+// parsing out the instructions
+var instructions = puzzleInput.split("\n\n")[1].split("\n");
+var digitInstructions = [];
+// converting strings to digits
+for (var i = 0; i < instructions.length; i++) {
+    var numberToMove = Number(instructions[i].charAt(5) + instructions[i].charAt(6));
+    var location_1 = Number(instructions[i].charAt(12) + instructions[i].charAt(13));
+    var target = Number(instructions[i].charAt(17) + instructions[i].charAt(18));
+    digitInstructions[i] = [numberToMove, location_1, target];
+}
+// Carringing out instructions
 for (var i = 0; i < digitInstructions.length; i++) {
     var numberToMove = digitInstructions[i][0];
     var from = digitInstructions[i][1] - 1;
     var target = digitInstructions[i][2] - 1;
+    //   // Part 1
+    //   for (let j = 0; j < numberToMove; j++) {
+    //     colsArrays[target].push(colsArrays[from][colsArrays[from].length - 1]);
+    //     colsArrays[from].pop();
+    //   }
+    var blockToMove = colsArrays[from].slice(Math.max(colsArrays[from].length - numberToMove));
     for (var j = 0; j < numberToMove; j++) {
-        colsArrays[target].push(colsArrays[from][colsArrays[from].length - 1]);
-        colsArrays[from].pop();
+        colsArrays[target].push(blockToMove[j]);
+        colsArrays[from].pop(numberToMove);
     }
 }
 function getLastItem(arr) {
@@ -51,6 +59,4 @@ function getLastItem(arr) {
     });
     return final;
 }
-console.log(digitInstructions);
-console.log(colsArrays);
 console.log(getLastItem(colsArrays));
--- index.ts
+++ index.ts
@@ -1,28 +1,21 @@
-const puzzleInput = `    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
+import puzzleInput from "./puzzleInput.js";
 
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2`;
+// // Dummy data
+// const puzzleInput = `    [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
 
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2`;
+
+// Sorting toers into arrays
 let cols = puzzleInput.split("\n\n")[0].split("\n");
-let colsArrays = new Array(cols.length - 1);
-for (let i = 0; i < cols.length - 1; i++) {
+let colsArrays = new Array(cols.length);
+for (let i = 0; i < cols.length; i++) {
   colsArrays[i] = [];
-}
-
-let instructions = puzzleInput.split("\n\n")[1].split("\n");
-let digitInstructions = [];
-
-for (let i = 0; i < instructions.length; i++) {
-  let numberToMove = Number(instructions[i].charAt(5));
-  let location = Number(instructions[i].charAt(12));
-  let target = Number(instructions[i].charAt(17));
-
-  digitInstructions[i] = [numberToMove, location, target];
 }
 
 let numberOfColumns = Number(
@@ -30,20 +23,6 @@ let numberOfColumns = Number(
     .replace(/\s/g, "")
     .charAt(cols[cols.length - 1].replace(/\s/g, "").length - 1)
 );
-
-// for (let i = 0; i < cols.length - 1; i++) {
-//   if (cols[i].charAt(1) !== " ") {
-//     colsArrays[0].unshift(cols[i].charAt(1));
-//   }
-
-//   if (cols[i].charAt(5) !== " ") {
-//     colsArrays[1].unshift(cols[i].charAt(5));
-//   }
-
-//   if (cols[i].charAt(9) !== " ") {
-//     colsArrays[2].unshift(cols[i].charAt(9));
-//   }
-// }
 
 // sorts columns into arrays
 for (let i = 0; i < cols.length - 1; i++) {
@@ -56,14 +35,43 @@ for (let i = 0; i < cols.length - 1; i++) {
   }
 }
 
+// parsing out the instructions
+let instructions = puzzleInput.split("\n\n")[1].split("\n");
+let digitInstructions = [];
+
+// converting strings to digits
+for (let i = 0; i < instructions.length; i++) {
+  let numberToMove = Number(
+    instructions[i].charAt(5) + instructions[i].charAt(6)
+  );
+
+  let location = Number(
+    instructions[i].charAt(12) + instructions[i].charAt(13)
+  );
+  let target = Number(instructions[i].charAt(17) + instructions[i].charAt(18));
+
+  digitInstructions[i] = [numberToMove, location, target];
+}
+
+// Carringing out instructions
 for (let i = 0; i < digitInstructions.length; i++) {
   let numberToMove = digitInstructions[i][0];
   let from = digitInstructions[i][1] - 1;
   let target = digitInstructions[i][2] - 1;
 
+  //   // Part 1
+  //   for (let j = 0; j < numberToMove; j++) {
+  //     colsArrays[target].push(colsArrays[from][colsArrays[from].length - 1]);
+  //     colsArrays[from].pop();
+  //   }
+
+  let blockToMove = colsArrays[from].slice(
+    Math.max(colsArrays[from].length - numberToMove)
+  );
+
   for (let j = 0; j < numberToMove; j++) {
-    colsArrays[target].push(colsArrays[from][colsArrays[from].length - 1]);
-    colsArrays[from].pop();
+    colsArrays[target].push(blockToMove[j]);
+    colsArrays[from].pop(numberToMove);
   }
 }
 
@@ -75,6 +83,4 @@ function getLastItem(arr) {
   return final;
 }
 
-console.log(digitInstructions);
-console.log(colsArrays);
 console.log(getLastItem(colsArrays));
