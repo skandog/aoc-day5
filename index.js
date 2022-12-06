283971021542bff0e591f682d55c90dc3724064f
skandog <muumrar@gmail.com>
2022-12-06 15:44:13 +0000

    written function to sort columns into arrays, finally, found that tricky

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var puzzleInput = "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2";
-var instructions = puzzleInput.split("\n\n")[1].split("\n");
 var cols = puzzleInput.split("\n\n")[0].split("\n");
 var colsArrays = new Array(cols.length - 1);
+for (var i = 0; i < cols.length - 1; i++) {
+    colsArrays[i] = [];
+}
+console.log(colsArrays);
+var instructions = puzzleInput.split("\n\n")[1].split("\n");
 var digitInstructions = [];
 for (var i = 0; i < instructions.length; i++) {
     var numberToMove = Number(instructions[i].charAt(5));
@@ -12,8 +16,28 @@ for (var i = 0; i < instructions.length; i++) {
 var numberOfColumns = Number(cols[cols.length - 1]
     .replace(/\s/g, "")
     .charAt(cols[cols.length - 1].replace(/\s/g, "").length - 1));
-for (var i = 0; i < numberOfColumns; i++) {
-    // Got to work out how to maintain the column structure
-    // but parse into separate arrays.
+// for (let i = 0; i < cols.length - 1; i++) {
+//   if (cols[i].charAt(1) !== " ") {
+//     colsArrays[0].unshift(cols[i].charAt(1));
+//   }
+//   if (cols[i].charAt(5) !== " ") {
+//     colsArrays[1].unshift(cols[i].charAt(5));
+//   }
+//   if (cols[i].charAt(9) !== " ") {
+//     colsArrays[2].unshift(cols[i].charAt(9));
+//   }
+// }
+for (var i = 0; i < cols.length - 1; i++) {
+    for (var j = 0; j < numberOfColumns; j++) {
+        var func = 4 * j + 1;
+        if (cols[i].charAt(func) !== " ") {
+            colsArrays[j].unshift(cols[i].charAt(func));
+        }
+    }
 }
+// for (let i = 0; i < numberOfColumns; i++) {
+//   // Got to work out how to maintain the column structure
+//   // but parse into separate arrays.
+//   colsArrays[i] = [];
+// }
 console.log(colsArrays);
--- index.ts
+++ index.ts
@@ -8,10 +8,14 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2`;
 
-let instructions = puzzleInput.split("\n\n")[1].split("\n");
 let cols = puzzleInput.split("\n\n")[0].split("\n");
-
 let colsArrays = new Array(cols.length - 1);
+for (let i = 0; i < cols.length - 1; i++) {
+  colsArrays[i] = [];
+}
+console.log(colsArrays);
+
+let instructions = puzzleInput.split("\n\n")[1].split("\n");
 let digitInstructions = [];
 
 for (let i = 0; i < instructions.length; i++) {
@@ -28,9 +32,34 @@ let numberOfColumns = Number(
     .charAt(cols[cols.length - 1].replace(/\s/g, "").length - 1)
 );
 
-for (let i = 0; i < numberOfColumns; i++) {
-  // Got to work out how to maintain the column structure
-  // but parse into separate arrays.
+// for (let i = 0; i < cols.length - 1; i++) {
+//   if (cols[i].charAt(1) !== " ") {
+//     colsArrays[0].unshift(cols[i].charAt(1));
+//   }
+
+//   if (cols[i].charAt(5) !== " ") {
+//     colsArrays[1].unshift(cols[i].charAt(5));
+//   }
+
+//   if (cols[i].charAt(9) !== " ") {
+//     colsArrays[2].unshift(cols[i].charAt(9));
+//   }
+// }
+
+for (let i = 0; i < cols.length - 1; i++) {
+  for (let j = 0; j < numberOfColumns; j++) {
+    let func = 4 * j + 1;
+
+    if (cols[i].charAt(func) !== " ") {
+      colsArrays[j].unshift(cols[i].charAt(func));
+    }
+  }
 }
+
+// for (let i = 0; i < numberOfColumns; i++) {
+//   // Got to work out how to maintain the column structure
+//   // but parse into separate arrays.
+//   colsArrays[i] = [];
+// }
 
 console.log(colsArrays);
