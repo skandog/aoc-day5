fb684204b1022bd7bfccc0a58bb8c1a61a60b185
skandog <muumrar@gmail.com>
2022-12-06 10:53:41 +0000

    set up for loop to parse out the columns, but got to think of how

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-
-
 const puzzleInput = `    [D]    
 [N] [C]    
 [Z] [M] [P]
@@ -8,26 +6,31 @@ const puzzleInput = `    [D]
 move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
-move 1 from 1 to 2`
+move 1 from 1 to 2`;
 
-let instructions = puzzleInput.split('\n\n')[1].split('\n')
-let cols = puzzleInput.split('\n\n')[0].split('\n')
+let instructions = puzzleInput.split("\n\n")[1].split("\n");
+let cols = puzzleInput.split("\n\n")[0].split("\n");
 
-let colsArrays = new Array(cols.length - 1)
+let colsArrays = new Array(cols.length - 1);
 let digitInstructions = [];
 
-for (let i = 0; i < instructions.length; i++) {   
-    let numberToMove = Number(instructions[i].charAt(5))
-    let location = Number(instructions[i].charAt(12))
-    let target = Number(instructions[i].charAt(17))
-    
-    digitInstructions[i] = [numberToMove, location, target]
+for (let i = 0; i < instructions.length; i++) {
+  let numberToMove = Number(instructions[i].charAt(5));
+  let location = Number(instructions[i].charAt(12));
+  let target = Number(instructions[i].charAt(17));
+
+  digitInstructions[i] = [numberToMove, location, target];
 }
 
-let numberOfColumns = cols[cols.length - 1].replace(/\s/g, "").charAt(cols[cols.length - 1].replace(/\s/g, "").length - 1)
-console.log(numberOfColumns);
+let numberOfColumns = Number(
+  cols[cols.length - 1]
+    .replace(/\s/g, "")
+    .charAt(cols[cols.length - 1].replace(/\s/g, "").length - 1)
+);
 
-//for (let i = 0; i < )
-
+for (let i = 0; i < numberOfColumns; i++) {
+  // Got to work out how to maintain the column structure
+  // but parse into separate arrays.
+}
 
 console.log(colsArrays);
