f5f6c6380dd3ba4d7135890413d2b7180ac6f027
skandog <muumrar@gmail.com>
2022-12-06 10:29:06 +0000

    parsed instructions into its own array

---
+++ index.ts
@@ -0,0 +1,18 @@
+
+
+const puzzleInput = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+
+let instructions = puzzleInput.split('\n\n')[1].split('\n')
+
+let digitInstructions = [];
+
+
+console.log(instructions);
